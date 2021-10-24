a17782cf889c152c600ab22595a8bda65c9c7175
oliviaortiz <93097536+oliviaortiz@users.noreply.github.com>
2021-10-24 16:33:26 -0500

    Add scripts and user counter to WavePortal.sol

--- Build_a_Web3_App_with_Solidity_Ethereum_Smart_Contracts/my-wave-portal/contracts/WavePortal.sol
+++ Build_a_Web3_App_with_Solidity_Ethereum_Smart_Contracts/my-wave-portal/contracts/WavePortal.sol
@@ -5,8 +5,29 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    mapping(address => uint256) userWaves;
+
     constructor() {
-        // console.log("I'm supposed to be smart, but I'm only printing a line...");
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("I'm supposed to be smart, but I'm only printing a line...");
+        // console.log("Yo yo, I am a contract and I am smart");
+    }
+    
+    function wave() public {
+        totalWaves += 1;
+        userWaves[msg.sender] += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
+    function getMyWaves() public view returns (uint256) {
+        uint256 uwaves = userWaves[msg.sender];
+        console.log("%s has waved %d times!", msg.sender, uwaves);
+        return uwaves;
+
     }
 }
