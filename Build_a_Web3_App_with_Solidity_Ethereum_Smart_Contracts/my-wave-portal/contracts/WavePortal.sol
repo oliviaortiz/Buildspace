badd04eb7728481492aad8398bde9f9ae712b799
oliviaortiz <93097536+oliviaortiz@users.noreply.github.com>
2021-10-24 15:47:04 -0500

    Add WavePortal contrac

---
+++ Build_a_Web3_App_with_Solidity_Ethereum_Smart_Contracts/my-wave-portal/contracts/WavePortal.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        // console.log("I'm supposed to be smart, but I'm only printing a line...");
+        console.log("Yo yo, I am a contract and I am smart");
+    }
+}
